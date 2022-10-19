b7c0e8abaa7093e46c6c505f2a57616a11e94c31
Aadarsh Unni Wilson <waadarsh@outlook.com>
2022-10-19 17:01:25 +0530

    Major Changes

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const cn = {
 };
 
 app.use(bodyParser.text());
+app.use(express.json());
 
 const db = pgp(cn);
 app.set("view engine", "ejs");
@@ -30,7 +31,7 @@ app.get('/', (req, res) => {
     db.any("SELECT image FROM test")
     .then((temp) => {
        //// const image1 = temp[1].image;
-        const temp1 = temp[1].image;
+        const temp1 = temp[2].image;
         //res.send(temp[0].image);
         //const temp2 = temp1.toString();
         console.log(temp1.toString().slice(1,-1));
@@ -41,11 +42,11 @@ app.get('/', (req, res) => {
 
         }).catch(error => res.send(error));        
 });
-app.get('/test', (req, res) => {
-    res.sendFile(path.join(__dirname + '/test.html'));
+app.get('/imgStoreAndRetrieve', (req, res) => {
+    res.sendFile(path.join(__dirname + '/imgStoreAndRetrieve.html'));
 });
 
-app.post('/test', (req, res) => {
+app.post('/imgStoreAndRetrieve', (req, res) => {
     const data = req.body;
     console.log(data);
     db.none('INSERT INTO test(image) VALUES ($1)', [data])
@@ -54,6 +55,29 @@ app.post('/test', (req, res) => {
     })
     .catch(error => console.log(error));
     
+});
+
+app.get('/cameraFunction', (req, res) => {
+    res.sendFile(path.join(__dirname + '/cameraFunction.html'));
+});
+
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname + '/test.html'));
+});
+app.post('/test', (req, res) => {
+    const data = req.body;
+    // // res.send(data);
+    // //const finalJSON = JSON.parse(data);
+    // //const dt2 = JSON.stringify(data);
+    // //console.log("data" + data);
+    // // db.none('INSERT INTO chklst_hdr(chklst_name,station_name,total_no_instruction) VALUES ($1,$2,$3)', [data.templateName, data.stationName, data.totalNoOfInstruction])
+    // // .then(() => {
+    // //     console.log("Success");
+    // // })
+    // // .catch(error => console.log(error));
+
+    console.log(typeof(data));
+    res.send("Test");
 });
 
 
